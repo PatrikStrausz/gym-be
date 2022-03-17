bce9d63ebc1804145993a5589a80162885c0430b
PatrikStrausz <patkolo1244@gmail.com>
2022-03-17 08:54:13 +0100

    Macros table added

---
+++ src/main/resources/db/migration/V6__macros.sql
@@ -0,0 +1,8 @@
+INSERT INTO macros(id, name)
+VALUES (1, 'Energy');
+INSERT INTO macros(id, name)
+VALUES (2, 'Protein');
+INSERT INTO macros(id, name)
+VALUES (3, 'Carbohydrates');
+INSERT INTO macros(id, name)
+VALUES (4, 'Fats');
